ff623d78f4b3fc14a638160e26dd541a5ac949df
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-08-21 21:40:07 +0530

    Create Day_0.py

---
+++ Day_0.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter the number:"))
+num2=int(input("Enter the number:"))
+num3=int(input("Enter the number:"))
+
+a=num1+num2
+b=num1+num3
+c=num2+num3
+
+print("\n",a,"\n",b,"\n",c)
