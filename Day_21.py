54bb85aca4da96cfc9ad4844cebc3bdc24b90ed5
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-08-24 22:24:02 +0530

    Create Day_21.py

---
+++ Day_21.py
@@ -0,0 +1,7 @@
+t=int(input())
+for _ in range(t):
+    salary=int(input())
+    if(salary<1500):
+        print(salary+(0.1*salary)+(0.9*salary))
+    else:
+        print(salary+(500)+(0.98*salary))
