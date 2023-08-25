3a5733fac21769befc3e7a657d614fff9bb8b70f
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-08-25 15:39:55 +0530

    Update Day_26.py

--- Day_26.py
+++ Day_26.py
@@ -1,6 +1,17 @@
+a)
 class Solution:
     def printTriangle(self, N):
         for i in range(N):
             for j in range(1,i+2):
                 print(j,end=" ")
             print()
+
+b)
+class Solution:
+    def printTriangle(self, N):
+        for i in range(N):
+            for j in range(1,i+2):
+                print(i+1,end=" ")
+            print()
+
+c)
