9f5f924831f5ade22fe1acdbab9824c3985a04f1
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-09-04 21:49:26 +0530

    Create Day_32.py

---
+++ Day_32.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, n):
+        # Code here
+        for i in range(0,2*n-1):
+            for j in range(0,2*n-1):
+                top = i
+                left = j
+                right = (2*n -2) - j
+                bottom = (2*n -2) - i
+                print(n- min(top,left,right,bottom),end=' ')
+            print()
