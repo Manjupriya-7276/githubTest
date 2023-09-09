9820a173d8edd2ccd7024db6e93eb253a63f8319
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-09-09 21:49:34 +0530

    Update Day_34.py

--- Day_34.py
+++ Day_34.py
@@ -16,3 +16,20 @@ class Solution:
             i += 6
 
         return 1
+B)
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x<0:
+            sign=-1
+        else:
+            sign=1
+        x=abs(x)
+        x_str=str(x)
+        rev_str=x_str[::-1].lstrip("0")
+        rev_int=int(rev_str)*sign
+        if (rev_int < -2**31 or rev_int>2**31-1):
+            return 0
+        else:
+            return rev_int
+
+        
