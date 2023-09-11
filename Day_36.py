53d343989a80daa1c074feb34c351d5781d0be48
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-09-11 18:31:54 +0530

    Update Day_36.py

--- Day_36.py
+++ Day_36.py
@@ -5,4 +5,21 @@ class Solution:
         for i in range(1,N+1):
             fact=fact*i
         return(fact)
-            
+ b)
+class Solution:
+    def fib(self, n: int) -> int:
+        a=0
+        b=1
+        i=2
+        if(n==0):
+            return 0
+        if(n==1):
+            return 1
+        else:
+            while i<=n:
+                c=a+b
+                a=b
+                b=c
+                i=i+1
+            return b
+
