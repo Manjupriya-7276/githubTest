4c9531cdeb47ce994e7fd32133bdc6ceed815df9
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-10-21 18:20:13 +0530

    Update Day_58.py

--- Day_58.py
+++ Day_58.py
@@ -43,3 +43,24 @@ class Solution:
         return count
 
 
+optimal approach :
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+       presum=0
+       mapp=defaultdict(int)
+       count=0
+       mapp[0]=1
+       for i in range(len(nums)):
+           presum=presum+nums[i]
+
+           remove=presum-k
+
+           count=count+mapp[remove]
+
+           mapp[presum] += 1
+
+       return count
+
+
+
