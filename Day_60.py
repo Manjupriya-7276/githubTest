f78473d858dfd5bd7d918abff50a99ab2001c4e8
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-10-23 22:40:54 +0530

    Update Day_60.py

--- Day_60.py
+++ Day_60.py
@@ -11,3 +11,36 @@ class Solution:
             if i!=min_value:
                 nums[i],nums[min_value]=nums[min_value],nums[i]
         return nums
+
+optimal approach(Quick sort)
+
+class Solution:
+    def pivot_place(self,nums,lowerbound,upperbound):
+        pivot=nums[lowerbound]
+        i=lowerbound+1
+        j=upperbound
+        while(True):
+            while i<=j and nums[i]<=pivot:
+                i=i+1
+            while i<=j and nums[j]>=pivot:
+                j=j-1
+            if i>j:
+                break
+            else:
+                nums[i],nums[j]=nums[j],nums[i]
+        nums[lowerbound],nums[j]=nums[j],nums[lowerbound]
+        return j
+
+    def quick_sort(self,nums,lowerbound,upperbound):
+        if(lowerbound<upperbound):
+            p=self.pivot_place(nums,lowerbound,upperbound)
+            self.quick_sort(nums,lowerbound,p-1)
+            self.quick_sort(nums,p+1,upperbound)
+
+    def sortColors(self, nums):
+        n=len(nums)
+        self.quick_sort(nums,0,n-1)
+        print(nums)
+
+
+        
