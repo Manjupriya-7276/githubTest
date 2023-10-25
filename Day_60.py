661a529004998b7df3bb73376ee8297b6b2a006c
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-10-25 21:29:40 +0530

    Update Day_60.py

--- Day_60.py
+++ Day_60.py
@@ -43,4 +43,47 @@ class Solution:
         print(nums)
 
 
+Better Approach:
+
+class Solution:
+    def sortColors(self, nums):
+        cnt0=0
+        cnt1=0
+        cnt2=0
+
+        for i in nums:
+            if i==0:
+                cnt0+=1
+            elif i==1:
+                cnt1+=1
+            elif i==2:
+                cnt2+=1
+
+        for i in range(cnt0):
+            nums[i]=0
+        for i in range(cnt0,cnt0+cnt1):
+            nums[i]=1
+        for i in range(cnt0+cnt1,len(nums)):
+            nums[i]=2
+
+Optimal Approach:
+
+class Solution:
+    def sortColors(self, nums):
+        low=0
+        mid=0
+        high=len(nums)-1
+        while(mid<=high):
+            if(nums[mid]==0):
+                nums[mid],nums[low]=nums[low],nums[mid]
+                low+=1
+                mid+=1
+            elif(nums[mid]==1):
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+
+
+
         
