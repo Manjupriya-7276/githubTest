ce7e5908897719ecc136e8aa4401f9968ff5c519
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-10-28 22:02:10 +0530

    Update Day_62.py

--- Day_62.py
+++ Day_62.py
@@ -11,4 +11,18 @@ class Solution:
                sums+=nums[j]
                maxi=max(sums,maxi)
         return maxi
-            
+        
+Optimal approach (Kadane's Algorithm)
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sums=0
+        maxi=-sys.maxsize-1
+        for i in range(len(nums)):
+            sums=sums+nums[i]
+            if sums > maxi:
+                maxi=sums
+            if sums<0:
+                sums=0
+        return maxi
+
