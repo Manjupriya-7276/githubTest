533b087e4babc18d46778785f85ba7ef727b3403
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-10-28 22:14:25 +0530

    Update Day_62.py

--- Day_62.py
+++ Day_62.py
@@ -26,3 +26,26 @@ class Solution:
                 sums=0
         return maxi
 
+
+To generate the maximum subarray
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sums=0
+        maxi=-sys.maxsize-1
+        start_index=-1
+        end_index=-1
+        for i in range(len(nums)):
+            if sums==0:
+                start=i
+            sums=sums+nums[i]
+            if sums > maxi:
+                maxi=sums
+                start_index=start
+                end_index=i
+            if sums<0:
+                sums=0
+        for i in range(start_index,end_index+1):
+            print(nums[i])
+        return maxi
+
+
