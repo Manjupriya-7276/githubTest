280749bd8a19a176c4d009fbbafe04f9b4862d4a
Manjupriya-72 <142822219+Manjupriya-72@users.noreply.github.com>
2023-11-21 23:25:20 +0530

    Update Day_76.py

--- Day_76.py
+++ Day_76.py
@@ -1,5 +1,5 @@
 Largest Subarray with 0 Sum
-
+Brutforce approach
 class Solution:
     def maxLen(self, n, arr):
         length=0
@@ -10,3 +10,20 @@ class Solution:
                 if s==0:
                     length=max(length,j-i+1)
         return length
+
+Optimal approach:
+class Solution:
+    def maxLen(self, n, arr):
+        hashmap={}
+        maxlen=0
+        s=0
+        for i in range(n):
+            s+=arr[i]
+            if s==0:
+                maxlen=max(maxlen,i+1)
+            else:
+                if s in hashmap:
+                    maxlen=max(maxlen,i-hashmap[s])
+                else:
+                    hashmap[s]=i
+        return maxlen
